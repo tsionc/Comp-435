b66f1a1d350aedcc4fccc85af90292cccd647ea3
tsionc <36338302+tsionc@users.noreply.github.com>
2019-09-07 11:56:40 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,8 +1,17 @@
 import os
 import sys
 
+from collections import Counter
 
 # opening and reading in file.
+sc= []
+cc=[]
 with open(sys.argv[1]) as f:
-    filecontent = f.read()
-    print (filecontent)
+    c=Counter(int(c.strip(),16) for c in f if int(c.strip(),16)) #for case-insensitive search
+    for  line in  c:
+        if c[line]>1:
+            
+            cc.append(line)
+            print (line, c[line] )
+    cc.sort()
+    print(c)
