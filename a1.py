ca43203e3209fcf78640eaac855aaf84ac3fe349
tsionc <36338302+tsionc@users.noreply.github.com>
2019-09-07 19:20:00 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,20 +1,17 @@
 import os
 import sys
-
 from collections import Counter
 
-# opening and reading in file.
-sc= []
+# opening and reading in the hex file as an integer and counting the lines that repeat.
+filecontent=Counter(int(filecontent,16) for filecontent in open(sys.argv[1])) 
 lineduplicat=[]
 
-    #print(f.read())
-filecontent=Counter(int(filecontent,16) for filecontent in open(sys.argv[1])) #for case-insensitive search
-    
-    #print(sorted(c.keys()))
+# first  it sorting the file in ascending order and its going to  go through the file and put the lines that are repeated 
+# and the number of times they have repated into a list
 for  line in  sorted(filecontent.keys()):
         if filecontent[line]>1:
             lineduplicat.append('%x' % line + " " + str(filecontent[line]))
-            
-for i in lineduplicat:
-     print(i)
+# go through the list and print out the output of lines one by one           
+for output in lineduplicat:
+     print(output)
 
